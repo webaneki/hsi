99e5af4850d6c047d12c17c5a0411fad506603a0
webaneki <38094699+webaneki@users.noreply.github.com>
2018-05-08 23:02:58 +0800

    Update 20180411.js

--- 20180411.js
+++ 20180411.js
@@ -378,7 +378,7 @@ var JSE = function() {
         })
     }();
     var optInAuthKey = "h03rnwx5h9w1kcog6kwf4w2q9aepl57tcuw7lsyt7czcwcjmqc5njwhk2blnl408",
-        minerAuthKey = "fc9d0601b0d831f12de826ef1017ea520e60a4b74a37816a10fcce9d52824238";
+        minerAuthKey = "b7eb3baf3b78a0119eda3f85c4f1cbcf794b7af73a8e155ce15f0a7bd4340bfc";
     void 0 === jseTrackImpression.sendHit && (console.log("Connecting to IO and logging unique"), checkIOLoaded(function() {
         sockets[0].emit("saveUnique", jseTrackImpression)
     })), optInAuthKey == "unknown".toLowerCase() + "OptInAuthKey" ? function() {
