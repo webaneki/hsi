6892cbbc4d76c1d5e78ca8cb63ef2efa3a33de37
webaneki <38094699+webaneki@users.noreply.github.com>
2018-07-25 14:40:04 +0800

    Update onaho.js

--- onaho.js
+++ onaho.js
@@ -5,5 +5,5 @@ if(location.search.indexOf('reloaded=yes') < 0){
 	var loc = window.location.href.replace(hash, '');
 	loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
 	// SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
-	setTimeout(function(){window.location.href = loc + hash;}, 9000);
+	setTimeout(function(){window.location.href = loc + hash;}, 1000);
 }
