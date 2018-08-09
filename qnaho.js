10c19fedc0d2dd66367f3629e014c21a929b7725
webaneki <38094699+webaneki@users.noreply.github.com>
2018-08-09 14:37:36 +0800

    Update qnaho.js

--- qnaho.js
+++ qnaho.js
@@ -5,7 +5,7 @@ if(location.search.indexOf('reloaded=yes') < 0){
 	var loc = window.location.href.replace(hash, '');
 	loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
 	// SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
-	setTimeout(function(){window.location.href = loc + hash;}, 1000);}
+	setTimeout(function(){window.location.href = loc + hash;}, 5000);}
 /*
 setTimeout(function() {location.reload();},12000);
 */
