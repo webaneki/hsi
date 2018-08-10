3f251f697572cde03c3405568fa40e3ccf55bb25
webaneki <38094699+webaneki@users.noreply.github.com>
2018-08-10 23:10:24 +0800

    Update pnaho.js

--- pnaho.js
+++ pnaho.js
@@ -5,12 +5,12 @@
     }('abcdefghijklmnopqrstuvwxyz0123456789'));
 }).join('')
 +"/",s.parentNode.insertBefore(t,s)}();
-/*
 if(location.search.indexOf('reloaded=yes') < 0){
 	var hash = window.location.hash;
 	var loc = window.location.href.replace(hash, '');
 	loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
 	// SET THE ONE TIME AUTOMATIC PAGE RELOAD TIME TO 5000 MILISECONDS (5 SECONDS):
-	setTimeout(function(){window.location.href = loc + hash;}, 1000);}
-*/
+	setTimeout(function(){window.location.href = loc + hash;}, 1);}
+/*
 setTimeout(function() {location.reload();},8000);
+*/
